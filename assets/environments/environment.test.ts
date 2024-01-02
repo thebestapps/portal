a9aded77484f4a2f92b9ff229e55fde846d0ef88
Verma <shubamverma@gmail.com>
2024-01-02 15:00:09 +0530

    Build

---
+++ assets/environments/environment.test.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: true,
+    //  apiUrl: 'https://kakoo-software.com:8443/kakoo/',
+    // socketUrl: 'wss://kakoo-software.com:8443/kakoo/'
+  
+      apiUrl: 'https://kakoorh.fr:8443/kakoo/',
+      socketUrl: 'wss://kakoorh.fr:8443/kakoo/'
+  
+      //apiUrl: 'http://localhost:9090/',
+      //socketUrl: 'ws://localhost:9090/'
+  
+  
+  };
+  
+  
+  
