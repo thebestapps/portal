702ff1f69d999af026e19119f7e53443eec01adc
Verma <shubamverma@gmail.com>
2024-01-03 13:30:58 +0530

    Build

---
+++ assets/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+
+  // apiUrl: 'http://localhost:8080/',
+  // apiUrl_v1: 'http://localhost:8080/kakoo-back-end/api/v1/',
+
+  // apiUrl: 'https://backend.kakoo-software.com/',
+  // apiUrl_v1: 'https://backend.kakoo-software.com/api/v1/',
+
+  // Staging = 1
+
+  // apiUrl: 'https://web.kakoo-software.com/kakoo-back-end/',
+  // apiUrl_v1: 'https://web.kakoo-software.com/kakoo-back-end/api/v1/',
+
+  //Dev
+  apiUrl: 'http://157.230.30.255:8080/kakoo-back-end/',
+  apiUrl_v1: 'http://157.230.30.255:8080/kakoo-back-end/api/v1/',
+};
