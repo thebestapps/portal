2fb23feb8a9c2e4164cfa753073faf640366afec
Verma <shubamverma@gmail.com>
2024-01-26 17:32:21 +0530

    Build

---
+++ assets/environments/environment.prod.ts
@@ -0,0 +1,28 @@
+export const environment = {
+  production: true,
+
+  // apiUrl: 'http://localhost:8080/',
+  // apiUrl_v1: 'http://localhost:8080/kakoo-back-end/api/v1/',
+
+  apiUrl: 'https://backend.kakoo-software.com/',
+  apiUrl_v1: 'https://backend.kakoo-software.com/api/v1/',
+
+  // Staging = 1
+
+  // apiUrl: 'https://web.kakoo-software.com/kakoo-back-end/',
+  // apiUrl_v1: 'https://web.kakoo-software.com/kakoo-back-end/api/v1/',
+
+  //Dev
+  // apiUrl: 'http://157.230.30.255:8080/kakoo-back-end/',
+  // apiUrl_v1: 'http://157.230.30.255:8080/kakoo-back-end/api/v1/',
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyDCg4LtIUpBuVN8R4GPYNXaDreGi-u1PjM',
+    authDomain: 'kakoo-software-staging.firebaseapp.com',
+    projectId: 'kakoo-software-staging',
+    storageBucket: 'kakoo-software-staging.appspot.com',
+    messagingSenderId: '1064112145139',
+    appId: '1:1064112145139:web:c21eee73a5889a41da6d86',
+    measurementId: 'G-P953THC65C',
+  },
+};
